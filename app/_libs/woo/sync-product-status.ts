0aa515a930d7544f6fea94ca6d488a32c520d8ec
Björn Asplund <bjuffe@gmail.com>
2026-05-25 21:27:49 +0200

    publish/unpublish in woo

--- app/_libs/woo/sync-product-status.ts
+++ app/_libs/woo/sync-product-status.ts
@@ -1,11 +1,19 @@
 // import * as productRepository from '~/_libs/core/repositories/product';
 
 import { woo_syncProductWithDataFromWoo } from '~/services/product.service';
-import { fetchProducts } from './products/fetch';
+import { fetchOne, fetchProducts } from './products/fetch';
 import type { WooProduct } from './products/types';
 import wooApiToProductUpsertData from './products/woo-api-to-product';
 
-export default async function syncWooProductStatus() {
+export async function syncOneWooProduct(wooProductId: number) {
+  const wooProduct = await fetchOne(wooProductId);
+
+  if (!wooProduct) return null;
+
+  return await wooApiToProductUpsertData(wooProduct);
+}
+
+export default async function syncAllWooProducts() {
   console.debug('FETCHING WOO PRODUCTS...');
 
   let wooProducts: WooProduct[] = await fetchProducts();
@@ -20,9 +28,9 @@ export default async function syncWooProductStatus() {
   let ignored = 0;
 
   for (const data of wooProducts) {
-    let upsertData = await wooApiToProductUpsertData(data);
+    const upsertData = await wooApiToProductUpsertData(data);
 
-    let res = await woo_syncProductWithDataFromWoo(upsertData);
+    const res = await woo_syncProductWithDataFromWoo(upsertData);
 
     if (res.result === 'updated') updated++;
     else if (res.result === 'notChanged') notChanged++;
